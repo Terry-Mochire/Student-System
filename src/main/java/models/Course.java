c3ed21599387dc466b418541f9e57d0470b293b0
Terry <terrybmochire@gmail.com>
2022-05-17 10:48:20 +0300

    Create basic courses pojo.

--- src/main/java/models/Course.java
+++ src/main/java/models/Course.java
@@ -1,6 +1,41 @@
 package models;
 
+import java.util.Objects;
+
 public class Course {
     private String courseName;
+    private int id;
 
+    public Course(String courseName){
+        this.courseName = courseName;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return id == course.id && courseName.equals(course.courseName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(courseName, id);
+    }
 }
