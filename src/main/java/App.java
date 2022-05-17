955c348c1846560c5ccfb704af7bb6076da64126
Terry <terrybmochire@gmail.com>
2022-05-17 20:26:56 +0300

    Refactor templates.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,8 @@
 import static spark.Spark.*;
 
+import Dao.Sql2oCourseDao;
 import Dao.Sql2oStudentDao;
+import models.Course;
 import models.Student;
 import org.sql2o.Sql2o;
 import spark.template.handlebars.HandlebarsTemplateEngine;
@@ -19,8 +21,8 @@ public class App {
         Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/student_system", "terry", "Postgres4041*");
 
         Sql2oStudentDao sql2oStudentDao = new Sql2oStudentDao(sql2o);
-
-        get("/", (request, response) -> {
+        Sql2oCourseDao sql2oCourseDao = new Sql2oCourseDao(sql2o);
+        get("/student/new", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
             return new ModelAndView(model, "studentform.hbs");
         }, new HandlebarsTemplateEngine());
@@ -40,11 +42,24 @@ public class App {
               return null;
         }, new HandlebarsTemplateEngine());
 
-        get("/students", (request, response) -> {
+        get("/courses", (request, response) -> {
             Map<String,Object> model = new HashMap<>();
             List<Student> students = sql2oStudentDao.getAll();
+            List<Course> courses = sql2oCourseDao.getAll();
             model.put("students", students);
-            return new ModelAndView(model, "allstudents.hbs");
+            model.put("courses", courses);
+            return new ModelAndView(model, "courses.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //show all students in a course
+
+        get("/students/:courseId", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            int courseId = Integer.parseInt(request.params("courseId"));
+            List<Student> studentsByCourse = sql2oStudentDao.findByCourseId(courseId);
+            System.out.println(studentsByCourse);
+            model.put("studentsByCourse", studentsByCourse);
+            return new ModelAndView(model, "success.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
