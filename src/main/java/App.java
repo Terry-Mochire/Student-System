919547e26bcabf04da2aea1adf8094e183c2403e
Terry <terrybmochire@gmail.com>
2022-05-19 14:25:34 +0300

    Final edits.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,12 +14,19 @@ import java.util.List;
 import java.util.Map;
 
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+    }
     public static void main(String[] args) {
-        port(2022);
-
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
+        //Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/student_system", "terry", "Postgres4041*");
 
-        Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/student_system", "terry", "Postgres4041*");
+        Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-52-3-2-245.compute-1.amazonaws.com:5432/d65d4k9v7tj9an", "olokzrhfenuuzy", "625b7d74bdc4aac854c13b0b59added35d1e7a2c1f3eb96fe078898881969281");
 
         Sql2oStudentDao sql2oStudentDao = new Sql2oStudentDao(sql2o);
         Sql2oCourseDao sql2oCourseDao = new Sql2oCourseDao(sql2o);
--- src/main/java/Database/DB.java
+++ src/main/java/Database/DB.java
@@ -3,6 +3,6 @@ package Database;
 import org.sql2o.*;
 
 public class DB {
-
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/student_system", "terry", "Postgres4041*");
+  // public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/student_system", "terry", "Postgres4041*");
+   public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-52-3-2-245.compute-1.amazonaws.com:5432/d65d4k9v7tj9an", "olokzrhfenuuzy", "625b7d74bdc4aac854c13b0b59added35d1e7a2c1f3eb96fe078898881969281");
 }
